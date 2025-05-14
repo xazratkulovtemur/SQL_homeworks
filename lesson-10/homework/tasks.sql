7493c20fce0684b50943c0b5bbb51c80e4ea034a
Temur <khazratkulovtemur@gmail.com>
2025-05-14 16:14:34 +0500

    lesson-10 updated

--- lesson-10/homework/tasks.sql
+++ lesson-10/homework/tasks.sql
@@ -2,13 +2,14 @@ CREATE TABLE Shipments (
     N INT PRIMARY KEY,
     Num INT
 );
-
 INSERT INTO Shipments (N, Num) VALUES
 (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1), (8, 1),
 (9, 2), (10, 2), (11, 2), (12, 2), (13, 2), (14, 4), (15, 4), 
 (16, 4), (17, 4), (18, 4), (19, 4), (20, 4), (21, 4), (22, 4), 
 (23, 4), (24, 4), (25, 4), (26, 5), (27, 5), (28, 5), (29, 5), 
 (30, 5), (31, 5), (32, 6), (33, 7);
+declare @c int;
+set @c = 40;
 
 ;with table1 as (
 	select ordinal as days
@@ -21,8 +22,14 @@ table2 as (
 	from table1 t1
 	left join Shipments s 
 	on t1.days=s.N
-	)
-select avg(Num*1.0) as Median
-from table2 
-where days in (20,21)
+	),
+table3 as (
+	select *
+	from table2
+	order by Num
+	offset (@c-1)/2 rows
+	fetch next 1+(1-@c%2) rows only
+)
+select avg(Num) as median
+from table3
 
